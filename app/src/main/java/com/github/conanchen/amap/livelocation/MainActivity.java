1508e3b56f5f575bcd8da22963628f4190a3edfa
conanchen <conan8chan@yahoo.com>
2018-01-17 17:59:53 +0800

    first commit

--- app/src/main/java/com/github/conanchen/amap/livelocation/MainActivity.java
+++ app/src/main/java/com/github/conanchen/amap/livelocation/MainActivity.java
@@ -48,7 +48,10 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         Dexter.withActivity(MainActivity.this)
                                 .withPermissions(Manifest.permission.ACCESS_FINE_LOCATION,
-                                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                                        Manifest.permission.ACCESS_COARSE_LOCATION
+                                        , Manifest.permission.READ_PHONE_STATE
+                                        , Manifest.permission.READ_EXTERNAL_STORAGE
+                                        , Manifest.permission.WRITE_EXTERNAL_STORAGE)
                                 .withListener(new MultiplePermissionsListener() {
                                     @Override
                                     public void onPermissionsChecked(MultiplePermissionsReport report) {
